8f3e9df4b097a76a81fd90ff56189415cf6a0284
alaktari <abdelkarimlaktari02@gmail.com>
2025-02-12 17:53:31 +0100

    Complited ex00 of module06.

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -6,14 +6,22 @@
 /*   By: alaktari <alaktari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 10:44:11 by alaktari          #+#    #+#             */
-/*   Updated: 2025/02/11 00:05:46 by alaktari         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:51:17 by alaktari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-#include "ScalarTypeCaster.hpp"
 
-ScalarConverter::ScalarConverter(std::string param)
+std::string	ScalarConverter::param = "";
+char		ScalarConverter::inChar = 0;
+int			ScalarConverter::inInt = 0;
+double		ScalarConverter::inDouble = 0.0;
+float		ScalarConverter::InFloat = 0.0f;
+bool		ScalarConverter::intFlag = false;
+bool		ScalarConverter::charFlag = false;
+bool		ScalarConverter::floatFlag = false;
+
+ScalarConverter::ScalarConverter(void)
 {
 	// std::cout << "ScalarConverter Constructor callefd\n";
 }
@@ -23,52 +31,154 @@ ScalarConverter::~ScalarConverter(void)
 	// std::cout << "ScalarConverter Destructor called\n";
 }
 
-ScalarConverter::ScalarConverter(const ScalarConverter& other)
+bool ScalarConverter::isChar(void)
 {
-	// std::cout << "Copy Constructor called\n";
+	if (param.empty())
+		return false;
+	size_t frontPoss = param.find_first_of('\'');
+	size_t BackPoss = param.find_last_of('\'');
+	if (!frontPoss && BackPoss == 2 && param.length() == 3)
+		return true;
+	return false;
 }
 
-ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other)
+bool ScalarConverter::isInt(void)
 {
-	// std::cout << "ScalarConverter Copy Assignment called\n";
-	return *this;
+	if (param.empty()) 
+		return false;
+	char *endPtr;
+	long value = std::strtol(param.c_str(), &endPtr, 10);
+	if (*endPtr != '\0' || value > INT_MAX || value < INT_MIN)
+		return false;
+	return true;
 }
 
-void	TypeChar(ScalarTypeCaster& helper, std::string param)
+bool ScalarConverter::isFloat(void)
 {
-	helper.setChar(param[1]);
-	// std::cout << "inChar: " << helper.getChar() << "\n";
+    if (param.empty())
+        return false;
+
+    char *endPtr;
+    float value = std::strtof(param.c_str(), &endPtr);
+
+    if (*endPtr == 'f' && *(++endPtr) == '\0')
+        return true;
+    if (*endPtr == '\0' && param.find('e') < param.length())
+        return true;
+    return false;
+}
+
+bool	ScalarConverter::isDouble(void)
+{
+    if (param.empty())
+        return false;
+
+    char *endPtr;
+    double value = std::strtod(param.c_str(), &endPtr);
+
+    if (*endPtr == '\0')
+        return true;
+    return false;
+}
+
+void	ScalarConverter::TypeChar(void)
+{
+	inChar = param[1];
+	
+	inInt = (static_cast<int>(inChar));
+	InFloat = static_cast<float>(inChar);
+	inDouble = static_cast<double>(inChar);
 	
 }
 
-void	TypeInt(ScalarTypeCaster& helper, std::string param)
+void	ScalarConverter::TypeInt(void)
 {
-	helper.setInt(atoi(param.c_str()));
-	// std::cout << "inInt: " << helper.getInt() << "\n";
+	inInt = atoi(param.c_str());
 	
+	if (inInt > 255 || inInt < 0)
+		charFlag = true;
+	else
+		inChar = static_cast<char>(inInt);
+	InFloat = static_cast<float>(inInt);
+	inDouble = static_cast<double>(inInt);
+}
+
+void	ScalarConverter::TypeFloat(void)
+{
+
+	std::cout << "it's float\n";
+
+	InFloat = std::strtof(param.c_str(), NULL);
+	double ckecker = InFloat;
+	
+	if (InFloat > 255 || InFloat < 0)
+		charFlag = true;
+	else
+		inChar = static_cast<char>(InFloat);
+	if (ckecker > INT_MAX || ckecker < INT_MIN)
+		intFlag = true;
+	else
+		InFloat = static_cast<int>(InFloat);
+	inDouble = static_cast<double>(InFloat);
+}
+
+void	ScalarConverter::TypeDouble(void)
+{
+	inDouble = std::strtod(param.c_str(), NULL);
+	
+	if (inDouble > 255 || inDouble < 0)
+		charFlag = true;
+	else
+		inChar = static_cast<char>(inDouble);
+	if (inDouble > INT_MAX || inDouble < INT_MIN)
+		intFlag = true;
+	else
+		inInt = static_cast<int>(inDouble);
+	if (inDouble > FLT_MAX || inDouble < -FLT_MAX)
+		floatFlag = true;
+	else
+		InFloat = static_cast<float>(inDouble);
+}
+
+void	ScalarConverter::displayer(void)
+{
+	std::cout << "char: ";
+	if (isprint(inChar) && !charFlag)
+		std::cout << '\'' << inChar << "'\n";
+	else if (!charFlag)
+		std::cout << "Non displayable\n";
+	else
+		std::cout << "impossible\n";
+	
+	std::cout << "int: ";
+	if (!intFlag)
+		std::cout << inInt << "\n";
+	else
+		std::cout << "impossible\n";
+		
+	std::cout << "float: ";
+	if (!floatFlag)
+		std::cout << std::fixed << InFloat << "f\n";
+	else
+		std::cout << "impossible\n";
+	std::cout << "Double: " << inDouble << "\n";
 	
 }
 
-void    ScalarConverter::convert(std::string param)
+int	ScalarConverter::convert(std::string _param)
 {
-	ScalarTypeCaster helper(param);
-
-	if (helper.isChar())
-	{
-		std::cout << "it's a character\n";
-		TypeChar(helper, param);
-	}
-	if (helper.isInt())
-	{
-		std::cout << "it's an interger\n";
-		TypeInt(helper, param);
-	}
-	// else if (helper.isFloat())
-	// {
-	// 	std::cout << "it's a float point number\n";
-	// }
-	// else if (helper.isDouble())
-	// {
-	// 	std::cout << "it's a double\n";
-	// }
+	param = _param;
+	
+	if (isChar())
+		TypeChar();
+	else if (isInt())
+		TypeInt();
+	else if (isFloat())
+		TypeFloat();
+	else if (isDouble())
+		TypeDouble();
+	else
+		return ((std::cout << "Invalid input\n"), 0);
+	displayer();
+	return 0;
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: alaktari <alaktari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 09:55:53 by alaktari          #+#    #+#             */
-/*   Updated: 2025/02/10 23:06:48 by alaktari         ###   ########.fr       */
+/*   Updated: 2025/02/12 17:51:22 by alaktari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,52 @@
 #include <iostream>
 #include <cstdlib>
 #include <climits>
+#include <iomanip>
+#include <cfloat>
 
 class ScalarConverter
 {
 	private:
+		static std::string	param;
+		static char		inChar;
+		static int			inInt;
+		static double		inDouble;
+		static float		InFloat;
+
+		static bool		intFlag;
+		static bool		charFlag;
+		static bool		floatFlag;
 		
-		ScalarConverter(std::string param);
+		ScalarConverter(void);
 		~ScalarConverter(void);
-		ScalarConverter(const ScalarConverter& other);
-		ScalarConverter& operator=(const ScalarConverter& other);
-	
+		
 	public:
-	
-
-		static void	convert(std::string param);
-		
 
 		
+		static bool	isChar(void);
+		static bool	isInt(void);
+		static bool	isFloat(void);
+		static bool	isDouble(void);
+        
+    	static char	getChar(void);
+		static int		getInt(void);
+		static float	getFloat(void);
+		static double	getDouble(void);
+
+		static void	setChar(char c);
+		static void	setInt(int i);
+		static void	setFloat(float f);
+		static void	setDouble(double d);
+
+		static void	TypeChar(void);
+		static void	TypeInt(void);
+		static void	TypeFloat(void);
+		static void	TypeDouble(void);
+
+
+		static void	displayer(void);
+
+		static int	convert(std::string param);
 };
 
 
