a4d365fc12ffacab5ade9a8af9a15ca1dde6cb07
alaktari <abdelkarimlaktari02@gmail.com>
2025-02-20 09:33:40 +0100

    Added a new test to ex00 of module05

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,11 +6,24 @@
 /*   By: alaktari <alaktari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 10:37:51 by alaktari          #+#    #+#             */
-/*   Updated: 2025/02/19 14:22:50 by alaktari         ###   ########.fr       */
+/*   Updated: 2025/02/20 09:33:19 by alaktari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
+
+void	Test(void)
+{
+	// ----- Test the two type of casting (c++/c style) ----
+	char c = 'a';
+    char *p = &c;
+
+    int *ptr1 = (int*)p;
+    // int *ptr2 = static_cast<int*>(p); // ==> Error
+
+    std::cout << "ptr1 => " << *ptr1 << "\n";
+    // std::cout << "ptr2 => " << *ptr2 << "\n";/
+}
 
 int main(int ac, char** av)
 {
@@ -21,4 +34,6 @@ int main(int ac, char** av)
 	}
 
 	ScalarConverter::convert(av[1]);
+
+	// Test();
 }
