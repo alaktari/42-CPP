fc6b3a0f6037162eab9e1fcdc70dad0bc63bed44
alaktari <abdelkarimlaktari02@gmail.com>
2025-02-27 09:02:40 +0000

    added a new test to ex02 of module 08.

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,11 +6,38 @@
 /*   By: alaktari <alaktari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 14:44:11 by alaktari          #+#    #+#             */
-/*   Updated: 2025/02/26 21:52:41 by alaktari         ###   ########.fr       */
+/*   Updated: 2025/02/27 09:02:31 by alaktari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+
+void	tests(void)
+{
+	MutantStack<std::string> mstack;
+
+	mstack.push("111111");
+	mstack.push("222222");
+	mstack.push("3333333");
+	mstack.push("5555555");
+	mstack.pop();
+	mstack.push("4444444");
+
+	MutantStack<std::string>::iterator it1 = mstack.begin();
+	MutantStack<std::string>::iterator it2 = mstack.end();
+
+	for (; it1 != it2; it1++)
+		std::cout << *it1 << " ";
+	std::cout << "\n";
+
+	it1--;
+	
+	if (it1 == it2)
+		std::cout << "reached the end of the conatainer\n";
+	else
+		std::cout << "not at the end";
+
+}
 
 int main()
 {
@@ -35,5 +62,8 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	tests();
+
 	return 0;
 }
